50af826edc99f653cc572608e4d8f452b1473d2c
“Brian <brian@lumeo.pro>
2012-05-02 21:33:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lumeo::Application.config.secret_token = '2890e9288feecb6a65d57d6f0375111f9c81eec0f0f14799e25fa6f832c9157de6e965fa6de624a1d388a76d07adacd5e4b671418a15e04765167b6be6ae2375'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lumeo::Application.config.session_store :cookie_store, key: '_lumeo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lumeo::Application.config.session_store :active_record_store
