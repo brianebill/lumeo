1d15e908926ccb2f0a3021e20aa919f36d988232
mikeycgto <mikeycgto@gmail.com>
2012-06-20 13:09:09 -0400

    updates to image job

--- lib/jobs/image_job.rb
+++ lib/jobs/image_job.rb
@@ -1,9 +1,9 @@
-class CourseImageJob < Struct.new(:id)
- def perform
-   course = Course.find(id)
-
-    if course
-      course.regenerate_styles!
+# Struct.new generates a new class with an "id" property
+# that we can inherit from
+class ImageJob < Struct.new(:id)
+  def perform
+    if image = Image.find(id)
+      image.regenerate_styles!
     end
- end
+  end
 end
