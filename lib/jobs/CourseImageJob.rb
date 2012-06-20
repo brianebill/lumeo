c3e97d124aafd636a965acda70d7ffd58bb83ea8
“Brian <brian@lumeo.pro>
2012-06-20 07:58:29 -0700

    stringify error

--- lib/jobs/CourseImageJob.rb
+++ lib/jobs/CourseImageJob.rb
@@ -1,8 +1,9 @@
-class ImageJob < Struct.new(:id)
-  def perform
-    # Note: this will throw an exception if an Image w/ this 'id' 
-    # does not exist
-    image = Image.find(id)
-    image.regenerate_styles!
-  end
+class CourseImageJob < Struct.new(:id)
+ def perform
+   course = Course.find(id)
+
+    if course
+      course.regenerate_styles!
+    end
+ end
 end
