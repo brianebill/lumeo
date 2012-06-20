6585d3ffd807349b4914cf72524ca79e6babbff0
“Brian <brian@lumeo.pro>
2012-06-20 12:48:37 -0700

    merging remote

--- db/migrate/20120614192210_add_request_id_to_assignment.rb
+++
@@ -1,6 +0,0 @@
-class AddRequestIdToAssignment < ActiveRecord::Migration
-  def change
-    add_column :assignments, :request_id, :integer
-    add_column :assignments, :tag_id, :integer
-  end
-end
