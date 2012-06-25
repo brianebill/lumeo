15592866129dca726034cf349a3cf79619ad4db6
mikeycgto <mikeycgto@gmail.com>
2012-06-25 14:17:37 -0400

    change private methods to protected. make vote action not private. also DRY things up a bit with find_request before filter

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  before_filter :find_request, :except => [:index, :new, :create]
+
   helper_method :sort_column, :sort_direction, :vote
   
   def index
@@ -8,7 +10,6 @@ class RequestsController < ApplicationController
   end
 
   def show
-    @request = Request.find(params[:id])
     @request = @commentable = Request.find(params[:id])
     respond_to do |format|
       format.html  # show.html.erb
@@ -16,77 +17,74 @@ class RequestsController < ApplicationController
     end
   end
 
-    def new
-      @request = Request.new
-      respond_to do |format|
-        format.html  # new.html.erb
-        format.json  { render :json => @request }
+  def new
+    @request = Request.new
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @request }
+    end
+  end
+
+  def create
+    @request = current_user.requests.build(params[:request])
+
+    respond_to do |format|
+      if @request.save
+        format.html  { redirect_to(@request,
+                                   :notice => 'Request was successfully created.') }
+        format.json  { render :json => @request,
+                       :status => :created, :location => @request }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @request.errors,
+                       :status => :unprocessable_entity }
       end
     end
+  end
 
-    def edit
-      @request = Request.find(params[:id])
-    end
-
-    def create
-      @request = current_user.requests.build(params[:request])
-
-      respond_to do |format|
-        if @request.save
-          format.html  { redirect_to(@request,
-                        :notice => 'Request was successfully created.') }
-          format.json  { render :json => @request,
-                        :status => :created, :location => @request }
-        else
-          format.html  { render :action => "new" }
-          format.json  { render :json => @request.errors,
-                        :status => :unprocessable_entity }
-        end
+  def update
+    respond_to do |format|
+      if @request.update_attributes(params[:request])
+        format.html  { redirect_to(@request,
+                                   :notice => 'Request was successfully updated.') }
+        format.json  { head :no_content }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @request.errors,
+                       :status => :unprocessable_entity }
       end
     end
+  end
 
-    def update
-      @request = Request.find(params[:id])
+  def destroy
+    @request.destroy
 
-      respond_to do |format|
-        if @request.update_attributes(params[:request])
-          format.html  { redirect_to(@request,
-                        :notice => 'Request was successfully updated.') }
-          format.json  { head :no_content }
-        else
-          format.html  { render :action => "edit" }
-          format.json  { render :json => @request.errors,
-                        :status => :unprocessable_entity }
-        end
-      end
+    respond_to do |format|
+      format.html { redirect_to requests_url }
+      format.json { head :no_content }
     end
+  end
 
-      def destroy
-        @request = Request.find(params[:id])
-        @request.destroy
+  def vote
+    if params[:up]
+      current_user.cast_vote(@request, 1)
+    else
+      current_user.cast_vote(@request, -1)
+    end
+    render :show
+  end
 
-        respond_to do |format|
-          format.html { redirect_to requests_url }
-          format.json { head :no_content }
-        end
-      end
-      
-      private
+  private
 
-      def sort_column
-        Request.column_names.include?(params[:sort]) ? params[:sort] : "title"
-      end
+  def find_request
+    @request = Request.find(params[:id])
+  end
 
-      def sort_direction
-        %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-      end
-      
-      def vote
-        if params[:up]
-          current_user.cast_vote(@request, 1)
-        else
-          current_user.cast_vote(@request, -1)
-        end
-        render :show
-       end
+  def sort_column
+    Request.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 end
