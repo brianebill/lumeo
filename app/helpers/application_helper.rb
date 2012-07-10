aa063a1183e956010b034ffa9ae9b1df145de55e
“Brian <brian@lumeo.pro>
2012-07-10 13:03:55 -0700

    home page refinements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,17 @@ module ApplicationHelper
       end
       link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
     end
+  
+  #display devise form on home page
+  def resource_name
+     :user
+   end
+
+   def resource
+     @resource ||= User.new
+   end
+
+   def devise_mapping
+     @devise_mapping ||= Devise.mappings[:user]
+   end
 end
